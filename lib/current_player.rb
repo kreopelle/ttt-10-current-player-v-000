3ee1b77da9530a7bb804138f72a16eb21bef2e90
Kayla Reopelle <kreopelle@gmail.com>
2018-03-22 23:28:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,25 @@
 def turn_count(board)
   turn_counter = 0
-  board.each do|space|
+  board.each do |index|
+
+
+    do|space|
     if board[space] == "X" || board[space] == "O"
       turn_counter += 1
     end
   end
 end
+
+## for (var i = 0; i < board.length; i++)
+
+# X = 1
+# if X > O
+# then it's O's turn
+# if O > X
+# then it's X's turn
+
+if counter % 2 == 0
+  x's turn
+else
+  o's turn
+  
