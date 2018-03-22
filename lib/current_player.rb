25ebf220467ce48e10ba6b6bd32e7806433ba81a
Kayla Reopelle <kreopelle@gmail.com>
2018-03-22 23:15:22 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,11 @@ def turn_count(board)
   x_counter = 0
   o_counter = 0
   board.each do |apple|
-    if |move| == "X"
-
+    if |apple| == "X"
+      x_counter += 1
+    elsif |apple| == "O"
+      o_counter += 1
+      
 
     end
   end
