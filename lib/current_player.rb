69a995d68189e38ec074646ea4dc58b9e647f35b
Kayla Reopelle <kreopelle@gmail.com>
2018-03-22 23:16:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,14 @@
 def turn_count(board)
   x_counter = 0
   o_counter = 0
+  turns_remaining = 9
   board.each do |apple|
     if |apple| == "X"
       x_counter += 1
     elsif |apple| == "O"
       o_counter += 1
-      
+    else
+      turns_remaining -= 1
 
     end
   end
