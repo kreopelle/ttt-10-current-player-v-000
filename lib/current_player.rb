14bdfe99c00b90626f89076343cbf2ae2e7367a4
Kayla Reopelle <kreopelle@gmail.com>
2018-03-22 23:20:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,8 @@
 def turn_count(board)
-  x_counter = 0
-  o_counter = 0
-  turns_remaining = 9
-  board.each do|apple|
-    if board[apple] == "X"
-      x_counter += 1
-    elsif board[apple] == "O"
-      o_counter += 1
-    else
-      turns_remaining -= 1
+  turn_counter = 0
+  board.each do|space|
+    if board[space] == "X" || board[space] == "O"
+      turn_counter += 1
     end
   end
 end
